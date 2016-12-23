50bf5761be3c20565bc47de0941ad1071a6a427f
Russell Briggs <russ@paraflyer.net>
2016-12-23 21:58:43 +1300

    Tests for NumberField

--- src/fields/__tests__/index.tests.ts
+++ src/fields/__tests__/index.tests.ts
@@ -444,6 +444,116 @@ describe('rev.fields', () => {
             return expect(test.validate(testModel, testMeta, 'create', result))
                 .to.eventually.have.property('valid', true);
         });
+    });
+
+    describe('NumberField', () => {
+        let testModel = {
+            value: <any> null
+        };
+        let testMeta = {
+            fields: [new fld.NumberField('value', 'Value')]
+        };
+        let result: ModelValidationResult;
+
+        beforeEach(() => {
+            result = new ModelValidationResult();
+        });
+
+        it('creates a field with properties as expected', () => {
+            let opts: fld.IFieldOptions = {};
+            let test = new fld.NumberField('value', 'Value', opts);
+            expect(test.name).to.equal('value');
+            expect(test.label).to.equal('Value');
+            expect(test.options).to.equal(opts);
+            expect(test).is.instanceof(fld.Field);
+        });
+
+        it('sets default field options if they are not specified', () => {
+            let test = new fld.NumberField('value', 'Value');
+            expect(test.options).to.deep.equal(fld.DEFAULT_FIELD_OPTIONS);
+        });
+
+        it('adds the numberValidator by default', () => {
+            let test = new fld.NumberField('value', 'Value', { required: false });
+            expect(test.validators.length).to.equal(1);
+            expect(test.validators[0]).to.equal(vld.numberValidator);
+        });
+
+        it('adds the required validator if options.required is true', () => {
+            let test = new fld.NumberField('value', 'Value', { required: true });
+            expect(test.validators.length).to.equal(2);
+            expect(test.validators[0]).to.equal(vld.requiredValidator);
+            expect(test.validators[1]).to.equal(vld.numberValidator);
+        });
+
+        it('adds the minValue validator if options.minValue is set', () => {
+            let test = new fld.NumberField('value', 'Value', { required: false, minValue: 'a' });
+            expect(test.validators.length).to.equal(2);
+            expect(test.validators[1]).to.equal(vld.minValueValidator);
+        });
+
+        it('adds the maxValue validator if options.maxValue is set', () => {
+            let test = new fld.NumberField('value', 'Value', { required: false, maxValue: 'z' });
+            expect(test.validators.length).to.equal(2);
+            expect(test.validators[1]).to.equal(vld.maxValueValidator);
+        });
+
+        it('successfully validates a number value', () => {
+            let test = new fld.NumberField('value', 'Value', { required: true });
+            testModel.value = 42;
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', true);
+        });
+
+        it('successfully validates a number value in a string', () => {
+            let test = new fld.NumberField('value', 'Value', { required: true });
+            testModel.value = '12.345';
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', true);
+        });
+
+        it('successfully validates a numeric value that passes validation', () => {
+            let test = new fld.NumberField('value', 'Value', {
+                required: true,
+                minValue: 40,
+                maxValue: 50
+            });
+            testModel.value = 42.123;
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', true);
+        });
+
+        it('successfully validates a null value if field not required', () => {
+            let test = new fld.NumberField('value', 'Value', { required: false });
+            testModel.value = null;
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', true);
+        });
+
+        it('does not validate on null value if field is required', () => {
+            let test = new fld.NumberField('value', 'Value', { required: true });
+            testModel.value = null;
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', false);
+        });
+
+        it('does not validate on non-numeric value', () => {
+            let test = new fld.NumberField('value', 'Value', { required: true });
+            testModel.value = 'I am a number, honest guv!...';
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', false);
+        });
+
+        it('does not validate a number if it does not match rules', () => {
+            let test = new fld.NumberField('value', 'Value', {
+                required: true,
+                minValue: 40,
+                maxValue: 50
+            });
+            testModel.value = 22;
+            return expect(test.validate(testModel, testMeta, 'create', result))
+                .to.eventually.have.property('valid', false);
+        });
 
     });
 
