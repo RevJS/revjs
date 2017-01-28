3f0bdaf3fc854a91471253d778f83d741800ee81
Russell Briggs <russ@paraflyer.net>
2017-01-28 18:02:38 +1300

    Update example to use Decorators

--- src/examples/01_defining_a_model.ts
+++ src/examples/01_defining_a_model.ts
@@ -1,9 +1,10 @@
 
 import '../polyfills';
-import * as rev from '../index';
+import * as m from '../index';
 
 // EXAMPLE:
 // import * as rev from 'rev-models'
+
 
 let TITLES = [
     ['Mr', 'Mr.'],
@@ -13,22 +14,20 @@ let TITLES = [
 ];
 
 export class Person {
-    title: string;
-    first_name: string;
-    last_name: string;
-    age: number;
 
-    email: string;
-    newsletter: boolean;
+    @m.SelectionField('Title', TITLES, { required: false })
+        title: string;
+    @m.TextField('First Name')
+        first_name: string;
+    @m.TextField('Last Name')
+        last_name: string;
+    @m.IntegerField('Age', { required: false })
+        age: number;
+
+    @m.EmailField('Email')
+        email: string;
+    @m.BooleanField('Registered for Newsletter?')
+        newsletter: boolean;
 }
 
-rev.register(Person, {
-    fields: [
-        new rev.SelectionField('title', 'Title', TITLES, { required: false }),
-        new rev.TextField('first_name', 'First Name'),
-        new rev.TextField('last_name', 'Last Name'),
-        new rev.IntegerField('age', 'Age', { required: false }),
-        new rev.EmailField('email', 'Email'),
-        new rev.BooleanField('newsletter', 'Registered for Newsletter?')
-    ]
-});
+m.register(Person);
