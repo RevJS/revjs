3b6cac7ee904bcc577850f9f3d392257bff8cae0
Russell Briggs <russ@paraflyer.net>
2017-03-25 12:35:47 +1300

    Add initial unit tests for getQueryObjectOperator()

--- packages/rev-models/src/queries/__tests__/operators.tests.ts
+++ packages/rev-models/src/queries/__tests__/operators.tests.ts
@@ -1,127 +1,42 @@
-
-import { getQueryObjectOperator } from '../operators';
-
-let query = {
-    a: 1,
-    b: 2,
-    $or: [
-        { c: 1}
-    ]
-};
-
-let parsed = getQueryObjectOperator(null, query);
-
-console.log(parsed);
-
-/*
 import { expect } from 'chai';
-import {
-    validateQuery,
-    validateQueryValue,
-    validateQueryValueList
-} from '../validation';
 
-describe('validateQuery()', () => {
+import * as d from '../../decorators';
+import { getQueryObjectOperator } from '../operators';
+import { initialiseMeta } from '../../models/meta';
 
-    it('throws if operator is not recognised', () => {
-        expect(() => {
-            validateQuery('wibble', null);
-        }).to.throw('unrecognised operator');
+class TestModel {
+    @d.IntegerField()
+        id: number;
+    @d.TextField()
+        name: string;
+    @d.BooleanField()
+        active: boolean;
+}
+
+const meta = initialiseMeta(TestModel);
+
+let invalidQueryObjects = [
+    null,
+    undefined,
+    [],
+    ['a'],
+    new Date(),
+];
+
+describe('getQueryObjectOperator() - parameters', () => {
+
+    it('throws if an invalid query object is passed', () => {
+        for (let val of invalidQueryObjects) {
+            expect(() => {
+                getQueryObjectOperator('wibble', meta);
+            }, 'value: ' + val).to.throw('is not a query object');
+        }
     });
 
-    it('does not throw for valid operator and query', () => {
+    it('does not throw for a valid query', () => {
         expect(() => {
-            validateQuery('$gt', 3);
+            getQueryObjectOperator({ name: 'bob' }, meta);
         }).to.not.throw();
     });
 
 });
-
-let validValues = [
-    '',
-    'aaa',
-    12,
-    123.456,
-    null,
-    true,
-    false,
-    new Date()
-];
-
-let invalidValues = [
-    new String('test'),  // tslint:disable-line
-    new Number(22),   // tslint:disable-line
-    undefined,
-    { test: 1 },
-    [1, 2, 3],
-    NaN
-];
-
-describe('validateQueryValue()', () => {
-
-    it('does not throw for valid values', () => {
-        for (let value of validValues) {
-            expect(() => {
-                validateQueryValue('$gt', value);
-            }).to.not.throw();
-        }
-    });
-
-    it('throws for invalid values', () => {
-        for (let value of invalidValues) {
-            expect(() => {
-                validateQueryValue('$gt', value);
-            }, 'value: ' + value).to.throw('invalid query value');
-        }
-    });
-
-});
-
-let validValueLists = [
-    [null],
-    ['aaa', 12],
-    [true, false],
-    [new Date()],
-    new Array<any>('a', 'b', 3)
-];
-
-let invalidValueLists = [
-    null,
-    [],
-    { test: 1 },
-    new Array()
-];
-
-let invalidValueListValues = [
-    ['a', undefined, 'b'],
-    [11, { test: 2}],
-    [NaN]
-];
-
-describe('validateQueryValueList()', () => {
-
-    it('does not throw for valid value lists', () => {
-        for (let value of validValueLists) {
-            expect(() => {
-                validateQueryValueList('$gt', value);
-            }).to.not.throw();
-        }
-    });
-
-    it('throws for invalid value lists', () => {
-        for (let value of invalidValueLists) {
-            expect(() => {
-                validateQueryValueList('$in', value);
-            }, 'value: ' + value).to.throw('must be an array of values');
-        }
-    });
-
-    it('throws for invalid values in value lists', () => {
-        for (let value of invalidValueListValues) {
-            expect(() => {
-                validateQueryValueList('$in', value);
-            }, 'value: ' + value).to.throw('invalid query value');
-        }
-    });
-});
-*/
