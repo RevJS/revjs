16462d0b0bc49683a0b40fbd1f12b4519eb6aebd
Russell Briggs <russ@paraflyer.net>
2017-04-02 11:02:17 +1200

    Fixed inmemory backend tests

--- packages/rev-models/src/backends/__tests__/inmemory.tests.ts
+++ packages/rev-models/src/backends/__tests__/inmemory.tests.ts
@@ -1,84 +1,82 @@
 
 import { expect } from 'chai';
 import { InMemoryBackend } from '../inmemory';
-import { IModelMeta, initialiseMeta } from '../../models/meta';
-import * as fld from '../../fields';
+import { initialiseMeta } from '../../models/meta';
 import { ModelOperationResult } from '../../operations/operationresult';
+import { Model } from '../../models/model';
+import * as d from '../../decorators';
+
+let GENDERS = [
+    ['male', 'Male'],
+    ['female', 'Female']
+];
+
+class TestModel extends Model {
+    @d.TextField()
+        name: string;
+    @d.IntegerField({ required: false })
+        age: number;
+    @d.SelectionField({ selection: GENDERS })
+        gender: string;
+    @d.BooleanField()
+        newsletter: boolean;
+    @d.DateField()
+        date_registered: Date;
+
+    getDescription?() {
+        return `${this.name}, age ${this.age}`;
+    }
+}
+
+initialiseMeta(TestModel);
+
+let testData: TestModel[] = [
+    new TestModel({
+        name: 'Joe Bloggs',
+        age: 20,
+        gender: 'male',
+        newsletter: true,
+        date_registered: new Date('2016-05-26')
+    }),
+    new TestModel({
+        name: 'Jane Doe',
+        age: 23,
+        gender: 'female',
+        newsletter: true,
+        date_registered: new Date('2017-01-01')
+    }),
+    new TestModel({
+        name: 'Felix The Cat',
+        age: 3,
+        gender: 'male',
+        newsletter: false,
+        date_registered: new Date('2016-12-03')
+    })
+];
 
 describe('rev.backends.inmemory', () => {
 
-    class TestModel {
-        name: string;
-        age: number;
-        gender: string;
-        newsletter: boolean;
-        date_registered: Date;
-
-        getDescription?() {
-            return `${this.name}, age ${this.age}`;
-        }
-    }
-
-    let testData: TestModel[] = [
-        {
-            name: 'Joe Bloggs',
-            age: 20,
-            gender: 'male',
-            newsletter: true,
-            date_registered: new Date('2016-05-26')
-        },
-        {
-            name: 'Jane Doe',
-            age: 23,
-            gender: 'female',
-            newsletter: true,
-            date_registered: new Date('2017-01-01')
-        },
-        {
-            name: 'Felix The Cat',
-            age: 3,
-            gender: 'male',
-            newsletter: false,
-            date_registered: new Date('2016-12-03')
-        }
-    ];
-
-    let GENDERS = [
-        ['male', 'Male'],
-        ['female', 'Female']
-    ];
-
-    let meta: IModelMeta;
     let backend: InMemoryBackend;
     let loadResult: ModelOperationResult<TestModel>;
     let readResult: ModelOperationResult<TestModel>;
 
     beforeEach(() => {
-        meta = {
-            fields: [
-                new fld.TextField('name'),
-                new fld.IntegerField('age', { required: false }),
-                new fld.SelectionField('gender', {selection: GENDERS}),
-                new fld.BooleanField('newsletter'),
-                new fld.DateField('date_registered')
-            ]
-        };
-        initialiseMeta(TestModel, meta);
+        TestModel.meta.singleton = false;
         backend = new InMemoryBackend();
-        loadResult = new ModelOperationResult<TestModel>({name: 'create'});
-        readResult = new ModelOperationResult<TestModel>({name: 'read'});
+        loadResult = new ModelOperationResult<TestModel>({operation: 'create'});
+        readResult = new ModelOperationResult<TestModel>({operation: 'read'});
     });
 
     describe('initial state - singleton model', () => {
 
         it('read() returns a model instance result with all undefined values', () => {
-            meta.singleton = true;
-            return backend.read(TestModel, meta, null, readResult)
+            TestModel.meta.singleton = true;
+            return backend.read(TestModel, null, readResult)
                 .then(() => {
                     expect(readResult.result).to.be.instanceOf(TestModel);
                     expect(readResult.results).to.be.null;
                     expect(readResult.result.getDescription).to.be.a('function');
-                    for (let field of meta.fields) {
+                    for (let field of TestModel.meta.fields) {
                         expect(readResult.result[field.name]).to.be.undefined;
                     }
                 });
@@ -89,7 +87,7 @@ describe('rev.backends.inmemory', () => {
     describe('initial state - non-singleton model', () => {
 
         it('read() returns an empty list', () => {
-            return backend.read(TestModel, meta, {}, readResult)
+            return backend.read(TestModel, {}, readResult)
                 .then(() => {
                     expect(readResult.result).to.be.null;
                     expect(readResult.results).to.be.instanceOf(Array);
@@ -104,8 +102,8 @@ describe('rev.backends.inmemory', () => {
         // TODO TEST LIMIT
 
         it('returns all records when where clause = {}', () => {
-            backend.load(testData, meta, loadResult);
-            return backend.read(TestModel, meta, {}, readResult)
+            backend.load(testData, TestModel, loadResult);
+            return backend.read(TestModel, {}, readResult)
                 .then(() => {
                     expect(readResult.result).to.be.null;
                     expect(readResult.results).to.deep.equal(testData);
