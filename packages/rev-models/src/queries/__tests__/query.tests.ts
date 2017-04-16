41cd808f2bcff0097302cbbeeb63d83d6a0bfc8b
Russell Briggs <russ@paraflyer.net>
2017-04-16 19:46:20 +1200

    Fix broken tests

--- packages/rev-models/src/queries/__tests__/query.tests.ts
+++ packages/rev-models/src/queries/__tests__/query.tests.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { isFieldValue, getLikeStrRegExp } from '../query';
+import { isFieldValue, getLikeOperatorRegExp } from '../query';
 
 let validFieldValues = [
     12,
@@ -37,38 +37,38 @@ describe('isFieldValue()', () => {
 describe('getLikeOperatorRegExp()', () => {
 
     it('returns an empty string matcher for empty strings', () => {
-        let r = getLikeStrRegExp('');
+        let r = getLikeOperatorRegExp('');
         expect(r.toString()).to.equal('/^.{0}$/m');
     });
 
     it('returns a simple string as an unescaped regex', () => {
-        let r = getLikeStrRegExp('i have no special chars');
+        let r = getLikeOperatorRegExp('i have no special chars');
         expect(r.toString()).to.equal('/^i have no special chars$/m');
     });
 
     it('escapes special regex characters', () => {
-        let r = getLikeStrRegExp('*some* [$pecial] ^{C}harac|ers?');
+        let r = getLikeOperatorRegExp('*some* [$pecial] ^{C}harac|ers?');
         expect(r.toString()).to.equal('/^\\*some\\* \\[\\$pecial\\] \\^\\{C\\}harac\\|ers\\?$/m');
     });
 
     it('replaces single % signs with .*', () => {
-        let r = getLikeStrRegExp('%contains % this%');
+        let r = getLikeOperatorRegExp('%contains % this%');
         expect(r.toString()).to.equal('/^.*contains .* this.*$/m');
     });
 
     it('replaces double % signs with single %', () => {
-        let r = getLikeStrRegExp('we are 99%% sure this will 100%% work');
+        let r = getLikeOperatorRegExp('we are 99%% sure this will 100%% work');
         expect(r.toString()).to.equal('/^we are 99% sure this will 100% work$/m');
     });
 
     it('does all of the above things at once', () => {
-        let r = getLikeStrRegExp('% con|ain$ 100%% ^awesome!?');
+        let r = getLikeOperatorRegExp('% con|ain$ 100%% ^awesome!?');
         expect(r.toString()).to.equal('/^.* con\\|ain\\$ 100% \\^awesome!\\?$/m');
     });
 
     it('throws when value is not a string', () => {
         expect(() => {
-            getLikeStrRegExp(null as any);
+            getLikeOperatorRegExp(null as any);
         }).to.throw('Supplied value is not a string');
     });
 
