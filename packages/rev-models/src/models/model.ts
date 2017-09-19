4420239e8b49cb1a9dcefcfedf0622287662a5d3
Russell Briggs <russ@paraflyer.net>
2017-09-19 19:30:47 +1200

    validateAsync now formally part of the IModel interface

--- packages/rev-models/src/models/model.ts
+++ packages/rev-models/src/models/model.ts
@@ -2,7 +2,8 @@ import { IValidationContext } from '../operations/validate';
 
 export interface IModel {
     [fieldName: string]: any;
-    validate?(vc: IValidationContext): void | Promise<void>;
+    validate?(vc: IValidationContext): void;
+    validateAsync(vc: IValidationContext): Promise<void>;
 }
 
 export type ModelCtor = new(...args: any[]) => IModel;
