21be2372b7edb309958fa434234f0e7268aec1e6
Russell Briggs <russ@paraflyer.net>
2017-11-04 23:03:44 +1300

    ModelAction now supports type="post" to post form data as JSON

--- packages/rev-forms-materialui/src/actions/ModelAction.tsx
+++ packages/rev-forms-materialui/src/actions/ModelAction.tsx
@@ -7,12 +7,18 @@ import { IExecArgs, IExecOptions } from 'rev-models/lib/operations/exec';
 import { ModelManager } from 'rev-models';
 import { ModelForm } from '../forms/ModelForm';
 
+export type ModelActionType = 'post' | 'method'
+const defaultActionType: ModelActionType = 'method';
+
 export interface IModelActionProps {
     label: string;
-    method: string;
-    values?: any;
+    type?: ModelActionType,
+    method?: string;
     args?: IExecArgs;
+    url?: string;
     options?: IExecOptions;
+    onSuccess?: (result: Response) => void;
+    onFailure?: (error: Error) => void;
 }
 
 export interface IModelActionContext {
@@ -43,17 +49,49 @@ export class ModelAction extends React.Component<IModelActionProps> {
     }
 
     onAction() {
+        const actionType = this.props.type || defaultActionType;
         console.log('onAction', this);
-        let modelMeta = this.modelManager.getModelMeta(this.modelForm.props.model);
-        let model = this.modelManager.hydrate(modelMeta.ctor, this.modelForm.state.formValues);
-        console.log(model);
-        this.modelManager.exec(model, this.props.method, this.props.args, this.props.options)
-        .then((res) => {
-            console.log('exec result', res);
-        })
-        .catch((err) => {
-            console.log('exec failure', err);
-        });
+        console.log('type', actionType);
+
+        if (actionType == 'post') {
+            if (!this.props.url) {
+                throw new Error('ModelAction Error: you must specify the url property when type is "post"')
+            }
+            fetch(this.props.url, {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'same-origin',
+                body: JSON.stringify(this.modelForm.state.formValues)
+            })
+            .then((res) => {
+                if (res.status < 200 || res.status > 299) {
+                    throw new Error('Got status ' + res.status);
+                }
+                if (this.props.onSuccess) {
+                    this.props.onSuccess(res);
+                }
+            })
+            .catch((err) => {
+                if (this.props.onFailure) {
+                    this.props.onFailure(err);
+                }
+            });
+        }
+        else {
+            let modelMeta = this.modelManager.getModelMeta(this.modelForm.props.model);
+            let model = this.modelManager.hydrate(modelMeta.ctor, this.modelForm.state.formValues);
+            console.log(model);
+            this.modelManager.exec(model, this.props.method, this.props.args, this.props.options)
+            .then((res) => {
+                console.log('exec result', res);
+            })
+            .catch((err) => {
+                console.log('exec failure', err);
+            });
+        }
     }
 
     render() {
