46a964e880dc1114b46ac3e6bc5e21b760f05b53
Russell Briggs <russell.briggs@xero.com>
2017-12-19 19:19:45 +1300

    Add further integration tests for model list

--- packages/rev-api/src/graphql/query/__tests__/model_list.tests.ts
+++ packages/rev-api/src/graphql/query/__tests__/model_list.tests.ts
@@ -1,18 +1,18 @@
 
 import { expect } from 'chai';
 import { ModelApiManager } from '../../../api/manager';
-import { modelManager } from '../../__tests__/models.fixture';
+import * as models from '../../__tests__/models.fixture';
 import { graphql, GraphQLSchema } from 'graphql';
 import { getGraphQLSchema } from '../../schema';
 
-describe.only('GraphQL "query" type', () => {
+describe.only('GraphQL "query" type - model list', () => {
 
     describe('When no models are registered', () => {
         let api: ModelApiManager;
         let schema: GraphQLSchema;
 
         before(() => {
-            api = new ModelApiManager(modelManager);
+            api = new ModelApiManager(models.modelManager);
             schema = getGraphQLSchema(api);
         });
 
@@ -47,6 +47,89 @@ describe.only('GraphQL "query" type', () => {
             `;
             const result = await graphql(schema, query);
             expect(result.data.no_models).to.equal('No models have been registered for read access');
+        });
+
+    });
+
+    describe('When models are registered', () => {
+
+        let api: ModelApiManager;
+        let schema: GraphQLSchema;
+
+        before(() => {
+            api = new ModelApiManager(models.modelManager);
+            api.register(models.User, { operations: ['read'] });
+            api.register(models.Post, { operations: ['read'] });
+
+            schema = getGraphQLSchema(api);
+        });
+
+        it('they are available to query, and have the correct return type', async () => {
+            const query = `
+                query {
+                    __schema {
+                        queryType {
+                            fields {
+                                name,
+                                type { name }
+                            }
+                        }
+                    }
+                }
+            `;
+            const result = await graphql(schema, query);
+            expect(result.data.__schema.queryType.fields).to.have.length(2);
+            expect(result.data.__schema.queryType.fields[0].name).to.equal('User');
+            expect(result.data.__schema.queryType.fields[0].type.name).to.equal('User');
+            expect(result.data.__schema.queryType.fields[1].name).to.equal('Post');
+            expect(result.data.__schema.queryType.fields[1].type.name).to.equal('Post');
+        });
+
+    });
+
+    describe('When some models are not readable', () => {
+
+        let api: ModelApiManager;
+        let schema: GraphQLSchema;
+
+        before(() => {
+            api = new ModelApiManager(models.modelManager);
+            api.register(models.User, { operations: ['read'] });
+            api.register(models.Post, { operations: ['create'] });
+
+            schema = getGraphQLSchema(api);
+        });
+
+        it('only registers models that are readable', async () => {
+            const query = `
+                query {
+                    __schema {
+                        queryType {
+                            fields {
+                                name,
+                                type { name }
+                            }
+                        }
+                    }
+                }
+            `;
+            const result = await graphql(schema, query);
+            expect(result.data.__schema.queryType.fields).to.have.length(1);
+            expect(result.data.__schema.queryType.fields[0].name).to.equal('User');
+        });
+
+        it('attempts to read unreadable models fail', async () => {
+            const query = `
+                query {
+                    Post {
+                        title
+                    }
+                }
+            `;
+            const result = await graphql(schema, query);
+            expect(result.data).to.be.undefined;
+            expect(result.errors).to.have.length(1);
+            expect(result.errors[0].message).to.equal('Cannot query field "Post" on type "query".');
         });
 
     });
