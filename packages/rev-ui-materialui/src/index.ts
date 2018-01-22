89ff262081328994c20e248865f2f0a1aba0a9b3
Russell Briggs <russ@paraflyer.net>
2018-01-22 16:53:30 +1300

    Move unimplemented fields to a temporary folder

--- packages/rev-ui-materialui/src/index.ts
+++ packages/rev-ui-materialui/src/index.ts
@@ -2,10 +2,23 @@
 import { UI_COMPONENTS } from 'rev-ui/lib/config';
 import { MUIListView } from './views/MUIListView';
 import { MUIDetailView } from './views/MUIDetailView';
+import { MUITextField } from './fields/MUITextField';
 
 export function registerComponents() {
 
     UI_COMPONENTS.views.ListView = MUIListView;
     UI_COMPONENTS.views.DetailView = MUIDetailView;
+
+    UI_COMPONENTS.fields.DateField = MUITextField;
+    UI_COMPONENTS.fields.TimeField = MUITextField;
+    UI_COMPONENTS.fields.DateTimeField = MUITextField;
+    UI_COMPONENTS.fields.IntegerField = MUITextField;
+    UI_COMPONENTS.fields.NumberField = MUITextField;
+    UI_COMPONENTS.fields.BooleanField = MUITextField;
+    UI_COMPONENTS.fields.SelectField = MUITextField;
+    UI_COMPONENTS.fields.EmailField = MUITextField;
+    UI_COMPONENTS.fields.URLField = MUITextField;
+    UI_COMPONENTS.fields.PasswordField = MUITextField;
+    UI_COMPONENTS.fields.TextField = MUITextField;
 
 }
