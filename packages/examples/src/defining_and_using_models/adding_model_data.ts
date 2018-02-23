2de98c5597c0fade2ad1213f858342ea48798918
Russell Briggs <russell.briggs@xero.com>
2018-02-23 18:46:04 +1300

    Move rev-models examples into a seperate 'examples' package

---
+++ packages/examples/src/defining_and_using_models/adding_model_data.ts
@@ -0,0 +1,19 @@
+import { modelManager, City, Customer } from './creating_models';
+
+(async () => {
+
+    const city1 = new City({
+        name: 'Wellington'
+    });
+    const createResult = await modelManager.create(city1);
+
+    const customer1 = new Customer({
+        first_name: 'Bob',
+        last_name: 'Jones',
+        age: 35,
+        gender: 'M',
+        city: createResult.result
+    });
+    await modelManager.create(customer1);
+
+})();
---
+++ packages/examples/src/defining_and_using_models/creating_models.ts
@@ -0,0 +1,45 @@
+
+import {
+    AutoNumberField, TextField, SelectField, IntegerField,
+    RelatedModel, ModelManager, InMemoryBackend
+} from 'rev-models';
+
+// Define models
+
+export class City {
+    @AutoNumberField({ primaryKey: true })
+        id: number;
+    @TextField()
+        name: string;
+
+    constructor(data: Partial<City>) {
+        Object.assign(this, data);
+    }
+}
+
+export class Customer {
+    @AutoNumberField({ primaryKey: true })
+        id: number;
+    @TextField()
+        first_name: string;
+    @TextField()
+        last_name: string;
+    @IntegerField()
+        age: number;
+    @SelectField({ selection: [['M', 'Male'], ['F', 'Female']] })
+        gender: string;
+    @RelatedModel({ model: 'City' })
+        city: City;
+
+    constructor(data: Partial<Customer>) {
+        Object.assign(this, data);
+    }
+}
+
+// Create ModelManager
+
+export const modelManager = new ModelManager();
+modelManager.registerBackend('default', new InMemoryBackend());
+
+modelManager.register(City);
+modelManager.register(Customer);
