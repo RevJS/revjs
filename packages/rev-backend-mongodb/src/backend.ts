9b7453083d6ef57b8ba9a358cc1e2b42771edbc8
Russell Briggs <russ@paraflyer.net>
2018-03-04 19:09:04 +1300

    WIP - created a quick & dirty MongoDB Query converter - needs tests!

--- packages/rev-backend-mongodb/src/backend.ts
+++ packages/rev-backend-mongodb/src/backend.ts
@@ -8,12 +8,28 @@ import {
     ICreateMeta, ICreateOptions, IUpdateMeta, IUpdateOptions, IRemoveMeta,
     IRemoveOptions, IReadMeta, IReadOptions, IExecMeta, IExecOptions, IModelMeta
 } from 'rev-models/lib/models/types';
+import { QueryParser } from 'rev-models/lib/queries/queryparser';
+import { IQueryNode } from 'rev-models/lib/queries/types';
+import { ConjunctionNode, FieldNode, ValueOperator } from 'rev-models/lib/queries/nodes';
+import { getLikeOperatorRegExp } from 'rev-models/lib/queries/utils';
 
 export interface IMongoDBBackendConfig {
     url: string;
     dbName: string;
     options?: MongoClientOptions;
 }
+
+const FIELD_OPERATOR_MAPPING = {
+    eq: '$eq',
+    ne: '$ne',
+    gt: '$gt',
+    gte: '$gte',
+    lt: '$lt',
+    lte: '$lte',
+    like: '__like',
+    in: '$in',
+    nin: '$nin'
+};
 
 export class MongoDBBackend implements IBackend {
     client: MongoClient;
@@ -41,6 +57,59 @@ export class MongoDBBackend implements IBackend {
 
     private _getCollectionName(meta: IModelMeta<any>) {
         return meta.name;
+    }
+
+    private _convertConjunctionNode(srcNode: ConjunctionNode<any>, destNode: object) {
+        if (srcNode.children.length == 0) {
+            return;
+        }
+        const mongoOperator = (srcNode.operator == 'and' && '$and') || (srcNode.operator == 'or' && '$or');
+        if (!mongoOperator) {
+            throw new Error(`Conjunction Operator '${srcNode.operator}' not recognised.`);
+        }
+        destNode[mongoOperator] = srcNode.children.map((child) => {
+            const childDestNode = {};
+            this._convertQueryNode(child, childDestNode);
+            return childDestNode;
+        });
+    }
+
+    private _convertFieldNode(srcNode: FieldNode<any>, destNode: object) {
+        destNode[srcNode.fieldName] = {};
+        for (let valueNode of srcNode.children as Array<ValueOperator<any>>) {
+            let mongoOperator = FIELD_OPERATOR_MAPPING[valueNode.operator];
+            if (!mongoOperator) {
+                throw new Error(`Field Value Operator '${valueNode.operator}' not recognised.`);
+            }
+            let value = valueNode.value;
+            if (mongoOperator == '__like') {
+                mongoOperator = '$regex';
+                value = getLikeOperatorRegExp(value);
+            }
+            destNode[srcNode.fieldName][mongoOperator] = value;
+        }
+    }
+
+    private _convertQueryNode(srcNode: IQueryNode<any>, destNode: object) {
+        if (srcNode instanceof ConjunctionNode) {
+            this._convertConjunctionNode(srcNode, destNode);
+        }
+        else if (srcNode instanceof FieldNode) {
+            return this._convertFieldNode(srcNode, destNode);
+        }
+        else {
+            throw new Error(`Unrecognised Query Node: ${srcNode.constructor.name}`);
+        }
+
+    }
+
+    // TODO: Tests!!!
+    private _convertQuery<T extends IModel>(manager: ModelManager, model: new() => T, where: object) {
+        const parser = new QueryParser(manager);
+        const queryNode = parser.getQueryNodeForQuery(model, where);
+        const mongoQuery = {};
+        this._convertQueryNode(queryNode, mongoQuery);
+        return mongoQuery;
     }
 
     async create<T extends IModel>(manager: ModelManager, model: T, options: ICreateOptions, result: ModelOperationResult<T, ICreateMeta>): Promise<ModelOperationResult<T, ICreateMeta>> {
@@ -91,7 +160,11 @@ export class MongoDBBackend implements IBackend {
     async read<T extends IModel>(manager: ModelManager, model: new() => T, options: IReadOptions, result: ModelOperationResult<T, IReadMeta>): Promise<ModelOperationResult<T, IReadMeta>> {
         const meta = manager.getModelMeta(model);
         const colName = this._getCollectionName(meta);
-        const records = await this.db.collection(colName).find({}).toArray();
+
+        const mongoQuery = this._convertQuery(manager, model, options.where);
+        console.log('Mongo Query', JSON.stringify(mongoQuery));
+
+        const records = await this.db.collection(colName).find(mongoQuery).toArray();
         result.results = records.map((record) => manager.hydrate(meta.ctor, record));
         result.setMeta({
             offset: options.offset,
