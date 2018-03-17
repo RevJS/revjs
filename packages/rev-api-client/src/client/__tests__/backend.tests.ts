6667fd3dfba2acd4b635e8f0a3aadeced797d49e
Russell Briggs <russ@paraflyer.net>
2018-03-17 15:53:48 +1300

    WIP - rev-api-client should pass standard backend tests

--- packages/rev-api-client/src/client/__tests__/backend.tests.ts
+++ packages/rev-api-client/src/client/__tests__/backend.tests.ts
@@ -1,6 +1,10 @@
 
 import { expect } from 'chai';
 import { ModelApiBackend } from '../backend';
+import { ModelManager, InMemoryBackend } from 'rev-models';
+import { standardBackendTests, IBackendTestConfig } from 'rev-models/lib/backends/testsuite';
+import { registerModels } from 'rev-models/lib/backends/testsuite/models';
+import { getMockApiHttpClient } from '../__test_utils__/mockHttpClient';
 
 describe('ModelApiBackend', () => {
 
@@ -15,5 +19,27 @@ describe('ModelApiBackend', () => {
             new (ModelApiBackend as any)();
         }).to.throw('You must provide an apiUrl');
     });
+
+});
+
+describe('ModelApiBackend - RevJS Backend Tests', () => {
+    const config: IBackendTestConfig = {} as any;
+    let backend: ModelApiBackend;
+
+    before(async () => {
+        // Create an in-memory model manager for API data
+        const apiModelManager = new ModelManager();
+        apiModelManager.registerBackend('default', new InMemoryBackend());
+        registerModels(apiModelManager);
+
+        // Create a mock Axios client for querying the API
+        const mockHttpClient = getMockApiHttpClient(apiModelManager);
+
+        // Create the backend ready for testing
+        backend = new ModelApiBackend('/api', mockHttpClient);
+        config.backend = backend;
+    });
+
+    standardBackendTests('ModelApiBackend', config);
 
 });
