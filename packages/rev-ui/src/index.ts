0dce7433ce9394704812f101f4e761ce1ce0917a
Russell Briggs <russell.briggs@xero.com>
2018-03-23 17:45:33 +1300

    rev_ui - export required interfaces for custom UI components from the root

--- packages/rev-ui/src/index.ts
+++ packages/rev-ui/src/index.ts
@@ -3,10 +3,11 @@ export { ModelProvider } from './provider/ModelProvider';
 export { withModelManager, IModelManagerProp } from './provider/withModelManager';
 
 export { DetailView, IDetailViewContextProp } from './views/DetailView';
-export { ListView } from './views/ListView';
+export { ListView, IListViewComponentProps } from './views/ListView';
 export { withDetailViewContext } from './views/withDetailViewContext';
 
-export { Field } from './fields/Field';
+export { Field, IFieldComponentProps } from './fields/Field';
 export { PostAction } from './actions/PostAction';
 export { SaveAction } from './actions/SaveAction';
 export { RemoveAction } from './actions/RemoveAction';
+export { IActionComponentProps } from './actions/types';
