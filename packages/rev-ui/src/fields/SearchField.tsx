67c5ad6440146bd4406b7308bcc40bdb1f25d38c
Russell Briggs <russ@paraflyer.net>
2018-03-30 16:04:51 +1300

    Implement SearchField

---
+++ packages/rev-ui/src/fields/SearchField.tsx
@@ -0,0 +1,119 @@
+
+import * as React from 'react';
+import { fields } from 'rev-models';
+
+import { IModelProviderContext } from '../provider/ModelProvider';
+import { ISearchViewContextProp } from '../views/SearchView';
+import { withSearchViewContext } from '../views/withSearchViewContext';
+import { UI_COMPONENTS } from '../config';
+import { IStandardComponentProps, getStandardProps } from '../utils/props';
+
+/**
+ * @private
+ */
+export type ColspanOptions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+/**
+ * The `<SearchField />` component should be included in a `<SearchView />`.
+ * When passed the name of a model field, it will render an appropriate input
+ * field to allow the value of the field to be searched.
+ */
+export interface ISearchFieldProps extends IStandardComponentProps {
+
+    /**
+     * Model field name. Must correspond with a field on the model specified
+     * for the `<SearchView />` component
+     */
+    name: string;
+
+    /** How many columns to span for narrow screens (1-12). Default = 12 */
+    colspanNarrow?: ColspanOptions;
+
+    /** How many columns to span for normal screens (1-12). Default = 6 */
+    colspan?: ColspanOptions;
+
+    /** How many columns to span for wide screens (1-12). Default = 6 */
+    colspanWide?: ColspanOptions;
+
+    /**
+     * If you provide a React component to this property, it will be used
+     * instead of the component configured in [[UI_COMPONENTS]] for the
+     * corresponding Field type. This component will be passed
+     * [[ISearchFieldComponentProps]]
+     */
+    component?: React.ComponentType<ISearchFieldComponentProps>;
+}
+
+/**
+ * @private
+ */
+export interface ISearchFieldState {
+    criteria: object;
+}
+
+/**
+ * @private
+ */
+export interface ISearchFieldComponentProps extends IStandardComponentProps  {
+    field: fields.Field;
+    label: string;
+    colspanNarrow: ColspanOptions;
+    colspan: ColspanOptions;
+    colspanWide: ColspanOptions;
+    criteria: object;
+    onCriteriaChange: (value: any) => void;
+}
+
+class SearchFieldC extends React.Component<ISearchFieldProps & ISearchViewContextProp, ISearchFieldState> {
+
+    modelField: fields.Field;
+    fieldComponentName: string;
+
+    constructor(props: ISearchFieldProps & ISearchViewContextProp, context: IModelProviderContext & ISearchViewContextProp) {
+        super(props, context);
+        if (!this.props.searchViewContext) {
+            throw new Error('SearchField Error: must be nested inside a SearchView.');
+        }
+        const meta = this.props.searchViewContext.modelMeta;
+        if (!(props.name in meta.fieldsByName)) {
+            throw new Error(`SearchField Error: Model '${meta.name}' does not have a field called '${props.name}'.`);
+        }
+        this.modelField = meta.fieldsByName[props.name];
+        this.fieldComponentName = this.modelField.constructor.name;
+        if (!this.props.component && !UI_COMPONENTS.fields[this.fieldComponentName]) {
+            throw new Error(`SearchField Error: There is no UI_COMPONENT registered for field type '${this.fieldComponentName}'`);
+        }
+    }
+
+    onCriteriaChange(criteria: any) {
+        this.props.searchViewContext.where[this.modelField.name] = criteria;
+        this.setState({ criteria });
+    }
+
+    render() {
+
+        const ctx = this.props.searchViewContext;
+
+        let cProps: ISearchFieldComponentProps = {
+            field: this.modelField,
+            label: this.modelField.options.label || this.modelField.name,
+            colspanNarrow: this.props.colspanNarrow || 12,
+            colspan: this.props.colspan || 6,
+            colspanWide: this.props.colspanWide || this.props.colspan || 6,
+            criteria: ctx.where[this.modelField.name],
+            onCriteriaChange: (criteria) => this.onCriteriaChange(criteria)
+        };
+        const sProps = getStandardProps(this.props);
+
+        const Component = this.props.component || UI_COMPONENTS.fields[this.fieldComponentName];
+        return <Component {...cProps} {...sProps} />;
+
+    }
+
+}
+
+/**
+ * See [[IFieldProps]]
+ * @private
+ */
+export const SearchField = withSearchViewContext(SearchFieldC);
---
+++ packages/rev-ui/src/fields/__tests__/SearchField.tests.tsx
@@ -0,0 +1,257 @@
+
+import * as React from 'react';
+import * as sinon from 'sinon';
+import * as models from '../../__fixtures__/models';
+import { expect } from 'chai';
+import * as rev from 'rev-models';
+import { mount } from 'enzyme';
+import { ModelProvider } from '../../provider/ModelProvider';
+import { SearchField, ISearchFieldComponentProps } from '../SearchField';
+import { SearchView, ISearchViewContextProp } from '../../views/SearchView';
+import { withSearchViewContext } from '../../views/withSearchViewContext';
+
+describe('SearchField', () => {
+
+    describe('construction', () => {
+        let modelManager: rev.ModelManager;
+        let errorStub: sinon.SinonStub;
+
+        beforeEach(() => {
+            modelManager = models.getModelManager();
+            errorStub = sinon.stub(console, 'error');
+        });
+
+        afterEach(() => {
+            errorStub.restore();
+        });
+
+        it('throws error when not nested inside a SearchView', () => {
+            expect(() => {
+                mount(<SearchField name="title" />);
+            }).to.throw('must be nested inside a SearchView');
+        });
+
+        it('throws error when specified field does not exist on the model', () => {
+            expect(() => {
+                mount(<ModelProvider modelManager={modelManager}>
+                    <SearchView model="Post" onSearch={() => null}>
+                        <SearchField name="colour" />
+                    </SearchView>
+                </ModelProvider>);
+            }).to.throw(`Model 'Post' does not have a field called 'colour'`);
+        });
+
+    });
+
+    type SpyComponentProps = ISearchFieldComponentProps & ISearchViewContextProp;
+    let receivedProps: SpyComponentProps;
+
+    const SpyComponent = withSearchViewContext<ISearchFieldComponentProps>((props) => {
+        receivedProps = props;
+        return <p>SpyComponent</p>;
+    });
+
+    function resetSpyComponent() {
+        receivedProps = null;
+    }
+
+    describe('ISearchFieldComponentProps', () => {
+        let modelManager: rev.ModelManager;
+        let meta: rev.IModelMeta<models.Post>;
+
+        before(() => {
+            resetSpyComponent();
+            modelManager = models.getModelManager();
+            meta = modelManager.getModelMeta('Post');
+            const backend = modelManager.getBackend('default') as rev.InMemoryBackend;
+            backend.OPERATION_DELAY = 100;
+            mount(
+                <ModelProvider modelManager={modelManager}>
+                    <SearchView model="Post" onSearch={() => null}>
+                        <SearchField
+                            name="title"
+                            component={SpyComponent} />
+                    </SearchView>
+                </ModelProvider>
+            );
+        });
+
+        it('passes specified field object', () => {
+            const expectedField = meta.fieldsByName['title'];
+            expect(receivedProps.field).to.equal(expectedField);
+        });
+
+        it('passes label, which should equal the field label or field name', () => {
+            const f = meta.fieldsByName['title'];
+            const expectedLabel = f.options.label || f.name;
+            expect(receivedProps.label).to.equal(expectedLabel);
+        });
+
+        it('passes default colspans', () => {
+            expect(receivedProps.colspanNarrow).to.equal(12);
+            expect(receivedProps.colspan).to.equal(6);
+            expect(receivedProps.colspanWide).to.equal(6);
+        });
+
+        it('passes undefined criteria', () => {
+            expect(receivedProps.criteria).to.be.undefined;
+        });
+
+        it('passes event handlers', () => {
+            expect(receivedProps.onCriteriaChange).to.be.a('function');
+        });
+
+    });
+
+    describe('colspan properties', () => {
+        let modelManager: rev.ModelManager;
+
+        function doMount(component: any) {
+            modelManager = models.getModelManager();
+            mount(
+                <ModelProvider modelManager={modelManager}>
+                    <SearchView model="Post" onSearch={() => null}>
+                        {component}
+                    </SearchView>
+                </ModelProvider>
+            );
+        }
+
+        beforeEach(() => {
+            resetSpyComponent();
+        });
+
+        it('default colspans are 12, 6, 6', () => {
+            doMount(<SearchField name="title" component={SpyComponent} />);
+            expect(receivedProps.colspanNarrow).to.equal(12);
+            expect(receivedProps.colspan).to.equal(6);
+            expect(receivedProps.colspanWide).to.equal(6);
+        });
+
+        it('can override colspanNarrow', () => {
+            doMount(<SearchField name="title" component={SpyComponent}
+                colspanNarrow={6}
+            />);
+            expect(receivedProps.colspanNarrow).to.equal(6);
+            expect(receivedProps.colspan).to.equal(6);
+            expect(receivedProps.colspanWide).to.equal(6);
+        });
+
+        it('can override colspan, and it also overrides colspanWide', () => {
+            doMount(<SearchField name="title" component={SpyComponent}
+                colspan={4}
+            />);
+            expect(receivedProps.colspanNarrow).to.equal(12);
+            expect(receivedProps.colspan).to.equal(4);
+            expect(receivedProps.colspanWide).to.equal(4);
+        });
+
+        it('can override colspanWide', () => {
+            doMount(<SearchField name="title" component={SpyComponent}
+                colspanWide={4}
+            />);
+            expect(receivedProps.colspanNarrow).to.equal(12);
+            expect(receivedProps.colspan).to.equal(6);
+            expect(receivedProps.colspanWide).to.equal(4);
+        });
+
+        it('can override all colspans', () => {
+            doMount(<SearchField name="title" component={SpyComponent}
+                colspanNarrow={1}
+                colspan={2}
+                colspanWide={3}
+            />);
+            expect(receivedProps.colspanNarrow).to.equal(1);
+            expect(receivedProps.colspan).to.equal(2);
+            expect(receivedProps.colspanWide).to.equal(3);
+        });
+    });
+
+    describe('SearchField Component Configuration', () => {
+        let modelManager: rev.ModelManager;
+        let errorStub: sinon.SinonStub;
+
+        beforeEach(() => {
+            modelManager = models.getModelManager();
+            errorStub = sinon.stub(console, 'error');
+        });
+
+        afterEach(() => {
+            errorStub.restore();
+        });
+
+        it('throws render error when a field type does not have a component registered', () => {
+            class ModelWithUnknownField {
+                test: string;
+            }
+            class UnknownField extends rev.fields.Field {}
+            modelManager.register(ModelWithUnknownField, { fields: [
+                new UnknownField('test')
+            ]});
+
+            expect(() => {
+                mount(<ModelProvider modelManager={modelManager}>
+                    <SearchView model="ModelWithUnknownField" onSearch={() => null}>
+                        <SearchField name="test" />
+                    </SearchView>
+                </ModelProvider>);
+            }).to.throw(`SearchField Error: There is no UI_COMPONENT registered for field type \'UnknownField\'`);
+        });
+
+    });
+
+    describe('Standard Component Properties', () => {
+        let modelManager: rev.ModelManager;
+
+        before(() => {
+            resetSpyComponent();
+            modelManager = models.getModelManager();
+            mount(
+                <ModelProvider modelManager={modelManager}>
+                    <SearchView model="Post" onSearch={() => null}>
+                        <SearchField
+                            name="title"
+                            component={SpyComponent}
+                            style={{marginTop: 10}}
+                        />
+                    </SearchView>
+                </ModelProvider>
+            );
+        });
+
+        it('style prop is passed to rendered component', () => {
+            expect(receivedProps.style).to.deep.equal({marginTop: 10});
+        });
+
+    });
+
+    describe('Event Handlers', () => {
+        let modelManager: rev.ModelManager;
+
+        beforeEach(() => {
+            resetSpyComponent();
+            modelManager = models.getModelManager();
+            mount(
+                <ModelProvider modelManager={modelManager}>
+                    <SearchView model="Post" onSearch={() => null}>
+                        <SearchField
+                            name="title"
+                            component={SpyComponent} />
+                    </SearchView>
+                </ModelProvider>
+            );
+        });
+
+        describe('onCriteriaChange()', () => {
+
+            it('updates field criteria', () => {
+                expect(receivedProps.criteria).to.be.undefined;
+                receivedProps.onCriteriaChange({ _eq: 10 });
+                expect(receivedProps.criteria).to.deep.equal({ _eq: 10 });
+            });
+
+        });
+
+    });
+
+});
